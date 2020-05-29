e553f3a2221444dbf91c5006b2d0674dec3bcd97
KinzaSyed <kinzasyed247@gmail.com$   git config --global user.email kinzasyed247@gmail.com>
2020-05-30 02:00:23 +0500

    abc

---
+++ assignment3_kinzasyed/src/screens/indexScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Axios from 'axios';
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  componentDidMount(){
+    
+  }
+  
+  render() {
+    return (
+      <View>
+        <Text> index Screen </Text>
+
+      </View>
+    );
+  }
+}
+export default Index;
